d30d67a402f1b251bd357332e6f4575daaf70be9
MuhanadIbrahim <parhoegypt@gmail.com>
2024-03-24 03:10:14 +0200

    Change publish\_to value to string in pubspec.yaml
    Update sdk constraint in pubspec.yaml
    
    Add assets path in flutter section of pubspec.yaml
    
    Add Montserrat font in fonts section of pubspec.yaml
    
    Add flutter\_assets section in pubspec.yaml to generate app\_images.dart file for image assets.

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,60 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesAvart1
+  /// lib/assets/images/avart_1.svg
+  static const String assetsImagesAvart1 = "lib/assets/images/avart_1.svg";
+
+  /// Assets for assetsImagesAvart2
+  /// lib/assets/images/avart_2.svg
+  static const String assetsImagesAvart2 = "lib/assets/images/avart_2.svg";
+
+  /// Assets for assetsImagesBalance
+  /// lib/assets/images/Balance.svg
+  static const String assetsImagesBalance = "lib/assets/images/Balance.svg";
+
+  /// Assets for assetsImagesCardBackground
+  /// lib/assets/images/card_background.svg
+  static const String assetsImagesCardBackground = "lib/assets/images/card_background.svg";
+
+  /// Assets for assetsImagesCardBackgrounf1
+  /// lib/assets/images/card_backgrounf1.png
+  static const String assetsImagesCardBackgrounf1 = "lib/assets/images/card_backgrounf1.png";
+
+  /// Assets for assetsImagesDashboard
+  /// lib/assets/images/dashboard.svg
+  static const String assetsImagesDashboard = "lib/assets/images/dashboard.svg";
+
+  /// Assets for assetsImagesExpenses
+  /// lib/assets/images/expenses.svg
+  static const String assetsImagesExpenses = "lib/assets/images/expenses.svg";
+
+  /// Assets for assetsImagesIncome
+  /// lib/assets/images/income.svg
+  static const String assetsImagesIncome = "lib/assets/images/income.svg";
+
+  /// Assets for assetsImagesLogout
+  /// lib/assets/images/logout.svg
+  static const String assetsImagesLogout = "lib/assets/images/logout.svg";
+
+  /// Assets for assetsImagesMy
+  /// lib/assets/images/my.svg
+  static const String assetsImagesMy = "lib/assets/images/my.svg";
+
+  /// Assets for assetsImagesMyinvestments
+  /// lib/assets/images/Myinvestments.svg
+  static const String assetsImagesMyinvestments = "lib/assets/images/Myinvestments.svg";
+
+  /// Assets for assetsImagesSetting2
+  /// lib/assets/images/setting-2.svg
+  static const String assetsImagesSetting2 = "lib/assets/images/setting-2.svg";
+
+  /// Assets for assetsImagesStatistics
+  /// lib/assets/images/Statistics.svg
+  static const String assetsImagesStatistics = "lib/assets/images/Statistics.svg";
+
+  /// Assets for assetsImagesWalletAccount
+  /// lib/assets/images/walletAccount.svg
+  static const String assetsImagesWalletAccount = "lib/assets/images/walletAccount.svg";
+}
+
